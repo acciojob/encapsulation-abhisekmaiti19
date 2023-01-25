3c561217607e408a3a1200d887316b3d3540b01d
Abhisek Maiti <abhisekmaiti19@gmail.com>
2023-01-25 19:14:04 +0530

    hello

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -4,11 +4,11 @@ public class RWOnly {
 
     private String name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
